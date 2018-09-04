93246c87d5bdb3ebf22b8c6c8f40ffe58e80170e
Divyanshu <divyanshugairola@gmail.com>
2018-09-04 17:15:22 +0530

    java exp 3

---
+++ sem 5/java/java lab/src/exp_3_a.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class stack {
+	
+	int [] arr = new int[10];
+	int stack_top=-1;
+	Scanner scn = new Scanner(System.in);
+	public void pop() {
+		stack_top--;
+
+	}
+	public void push(int a) {
+		stack_top++;
+		arr[stack_top]=a;
+
+	}
+	public void top() {
+		System.out.println("top is at "+ this.stack_top);
+
+	}
+	public void display() {
+		System.out.println("elements in stack");
+		for(int i=stack_top;i>=0;i--) {
+			System.out.print(this.arr[i]+" ");
+		}
+
+	}
+}
+class stackuse extends stack {
+	 
+	public void display() {
+		super.display();
+	}
+	
+	
+	public void pop() {
+		super.pop();
+
+	}
+	public void push(int a) {
+		super.push(a);
+
+	}
+	public void top() {
+		super.top();
+
+	}
+	
+}
+
+public class exp_3 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		stackArray(scn);
+		
+	}
+	private static void stackArray(Scanner scn) {
+		stackuse s1=new stackuse();
+		s1.push(2);
+		s1.push(3);
+		s1.push(4);
+		s1.display();
+		s1.pop();
+		s1.pop();
+		s1.display();
+
+	}
+	
+
+
+}
+
---
+++ sem 5/java/java lab/src/exp_3_b.java
@@ -0,0 +1,38 @@
+class class1{
+		 class1() {
+			System.out.println("in class 1");
+		}
+		
+	}
+class class2 extends class1{
+		 class2() {
+			System.out.println("in class 2");
+		}
+	}
+class class3 extends class2{
+		 class3() {
+			System.out.println("in class 3");
+		}
+	}
+class class4 extends class1{
+	public class4() {
+		System.out.println("in class 4");
+	}
+	
+}
+
+public class inheritance {
+	
+	public static void main(String[] args) {
+		System.out.println("single level");
+		class2 ob2 = new class2();
+		System.out.println("\nmulti level");
+		class3 ob3 = new class3();
+		System.out.println("\nhierarchical");
+		class4 ob4 = new class4();
+		class2 ob1 = new class2();
+
+		
+	}
+
+}
